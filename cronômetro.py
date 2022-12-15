4c11a4fd9835b8e83ef65dce46bf4c0105b52ab2
JABA-NADA <119821825+JABA-NADA@users.noreply.github.com>
2022-12-14 23:58:52 -0300

    Update cronômetro.py

--- cronômetro.py
+++ cronômetro.py
@@ -1,14 +1,20 @@
 from time import sleep
+my_time = 1
 
 print('Welcome to your stopwatch!')
 sleep(1)
 
-my_time = int(input('Enter the time in seconds:'))
-sleep(0.3)
-
-while my_time:
-    for x in range(my_time, 0, -1):
-        print(f'{x}')
-        sleep(0.5)
+while True:
+    my_time += int(input('Enter the time in seconds:'))
+    sleep(0.3)
+    for x in reversed(range(0, my_time)):
+        seconds = x % 60
+        minutes = int(x / 60) % 60
+        hours = int(x / 3600)
+        print(f'{hours:02}:{minutes:02}:{seconds:02}')
+        sleep(1)
     print("Time's up!")
-    break
+    question = str(input('Do you wanna to continue [Y] or [N]?: ')).upper()
+    if question == 'N':
+        print('The end!')
+        break
